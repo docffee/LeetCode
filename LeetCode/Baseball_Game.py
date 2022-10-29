0e52bcb3d81381b0e6799c50d594d0340aedcf54
docffee <alvinduonglong@gmail.com>
2022-10-28 19:53:59 -0500

    First Commit

---
+++ LeetCode/Baseball_Game.py
@@ -0,0 +1,16 @@
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+        """Runtime: 44 ms, faster than 92.05% of Python3 online submissions for Baseball Game.
+        Memory Usage: 14.2 MB, less than 32.76% of Python3 online submissions for Baseball Game."""
+        num = ["1","2","3","4","5","6","7","8","9","0"]
+        arr = []
+        for i in operations:
+            if i.lstrip('-').isdigit():
+                arr.append(int(i))
+            elif i == '+':
+                arr.append(int(arr[-1])+int(arr[-2]))
+            elif i == 'D':
+                arr.append(2*int(arr[-1]))
+            elif i == 'C':
+                arr.pop(-1)
+        return sum(arr)
