ec611a61daa2a33fc252df3cca00ad3620525da8
docffee <alvinduonglong@gmail.com>
2022-11-26 18:24:38 -0600

    First Commit

---
+++ LeetCode/In_Order_Binary_Tree_Traversal.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+"""
+Runtime: 33 ms, faster than 91.51% of Python3 online submissions for Binary Tree Inorder Traversal.
+Memory Usage: 13.9 MB, less than 59.95% of Python3 online submissions for Binary Tree Inorder Traversal.
+"""
+class Solution:
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+        res, stack = [], [(root, False)]
+        while stack:
+            node, visited = stack.pop()  # the last element
+            if node:
+                if visited:
+                    res.append(node.val)
+                else:  # inorder: left -> root -> right
+                    stack.append((node.right, False))
+                    stack.append((node, True))
+                    stack.append((node.left, False))
+        return res
