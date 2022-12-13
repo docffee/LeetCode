2db5b20d6d072ada364b897d44333366bf27ecad
docffee <alvinduonglong@gmail.com>
2022-12-12 22:06:12 -0500

    First Commit

---
+++ LeetCode/Number_of_Students_Unable_to_Eat_Lunch.py
@@ -0,0 +1,13 @@
+class Solution:
+    def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
+        a =[0,0]
+        for st in students:
+            a[st]+=1
+        k = 0
+        while k < len(sandwiches):
+            if a[sandwiches[k]]>0:
+                a[sandwiches[k]]-=1
+            else:
+                break
+            k+=1
+        return len(sandwiches)-k
